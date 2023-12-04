3a4ea4363dc5ba37ba1f8ca7f98e117734f7ea2f
rabiaakkus <145451092+rabiaakkus@users.noreply.github.com>
2023-12-04 10:00:30 +0300

    Add files via upload

---
+++ sehir 1120/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace sehir_1120
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSehir = new System.Windows.Forms.Label();
+            this.txtSehir = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSehir
+            // 
+            this.lblSehir.AutoSize = true;
+            this.lblSehir.Location = new System.Drawing.Point(12, 18);
+            this.lblSehir.Name = "lblSehir";
+            this.lblSehir.Size = new System.Drawing.Size(46, 13);
+            this.lblSehir.TabIndex = 0;
+            this.lblSehir.Text = "sehir adı";
+            // 
+            // txtSehir
+            // 
+            this.txtSehir.Location = new System.Drawing.Point(64, 18);
+            this.txtSehir.Name = "txtSehir";
+            this.txtSehir.Size = new System.Drawing.Size(115, 20);
+            this.txtSehir.TabIndex = 1;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(15, 53);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(164, 73);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 171);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtSehir);
+            this.Controls.Add(this.lblSehir);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSehir;
+        private System.Windows.Forms.TextBox txtSehir;
+        private System.Windows.Forms.Button btnEkle;
+    }
+}
+
